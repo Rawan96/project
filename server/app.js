7fefb348b6cc1d1f781e93177f2ce8a6d0c66521
rawan <rawan.designer@hotmail.com>
2020-05-18 23:52:18 +0300

    Edit the app file

--- server/app.js
+++ server/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:4000",
+  origin: "http://localhost:4001",
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hiiiiiiiii");
