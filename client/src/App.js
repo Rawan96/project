86f7e431ebb6915dd9b9a0471a164a0f60cf4246
rawan <rawan.designer@hotmail.com>
2020-05-19 11:43:02 +0300

    Adding the navbar to the app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <a href="/posts">Blog</a>
+          <div>
+            <li>
+              <link to={"/posts"}>Posts</link>
+            </li>
+            <li>
+              <link to={"/add"}>Add</link>
+            </li>
+          </div>
+        </nav>
+      </div>
+    </Router>
   );
 }
 
