210616048034c03764629034367b59a28d7b8033
rawan <rawan.designer@hotmail.com>
2020-05-19 13:20:55 +0300

    Edit the app

--- server/app.js
+++ server/app.js
@@ -3,13 +3,6 @@ const cors = require("cors");
 
 const app = express();
 
-/*const corsOptions = {
-  origin: "http://localhost:4001",
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));*/
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
