cf61cda06ef89ad284d4a1c614a397868106c064
Rawan <rawan.designer@hotmail.com>
2020-05-19 13:22:12 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,12 +5,6 @@ const cors = require("cors");
 const app = express();
 const router = require("./routes");
 
-/*const corsOptions = {
-  origin: "http://localhost:4001",
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
