f692d1712248ba75422d544f53b3e213b4b67e75
rawan <rawan.designer@hotmail.com>
2020-05-21 03:15:56 +0300

    Edit the file

--- client/src/components/MainPage/index.js
+++ client/src/components/MainPage/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CardComponent from "../Card";
-import { Spin, Alert } from "antd";
 
 function MainPage() {
   const [error, setError] = useState();
